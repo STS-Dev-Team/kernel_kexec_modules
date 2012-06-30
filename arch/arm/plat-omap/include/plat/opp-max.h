6166196c79a338fc1dd2ecd2da0cf357bb5ba3a5
Jon Pry <jonpry@gmail.com>
2012-06-29 22:44:51 -0400

    initial commit. emu_uart module is functional

---
+++ arch/arm/plat-omap/include/plat/opp-max.h
@@ -0,0 +1,27 @@
+/*
+ * opp_cpcap.h - MAX-specific headers for the OPP code
+ *
+ * Copyright (C) 2010 Motorola.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+#ifndef _ARCH_ARM_PLAT_OMAP_OPP_MAX_H
+#define _ARCH_ARM_PLAT_OMAP_OPP_MAX_H
+
+#include <linux/kernel.h>
+#include <linux/device.h>
+#include "voltage.h"
+
+unsigned long omap_max8952_vsel_to_uv(const u8 vsel);
+u8 omap_max8952_uv_to_vsel(unsigned long uV);
+
+u8 omap_max8952_onforce_cmd(const u8 vsel);
+u8 omap_max8952_on_cmd(const u8 vsel);
+u8 omap_max8952_sleepforce_cmd(const u8 vsel);
+u8 omap_max8952_sleep_cmd(const u8 vsel);
+u8 omap_max8952_reconfigure_regs(void);
+
+#endif
---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sat Jun 23 20:56:36 CDT 2012"
+#define LINUX_COMPILE_TIME "20:56:36"
+#define LINUX_COMPILE_BY "jon"
+#define LINUX_COMPILE_HOST "galactica"
+#define LINUX_COMPILE_DOMAIN "prymfg.com"
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-50) "
